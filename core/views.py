f65ad018e63de4167e8e6ef249bc16d8bce4a0a3
rchicone <rodolfochicone@hotmail.com>
2016-09-29 22:30:10 -0300

    inclusão do app accounts

--- core/views.py
+++ core/views.py
@@ -4,13 +4,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
-from django.views.generic import View, TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import View, TemplateView, CreateView
+from django.contrib.auth import get_user_model
 
 from .forms import ContactForm
+
+
+User = get_user_model()
+
 
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
 
 index = IndexView.as_view()
 
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     #apps
     'core',
+    'accounts',
     'catalog',
 ]
 
@@ -143,6 +144,16 @@ EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'admin@djangoecommerce.com'
+
+# auth
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_URL = 'logout'
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend',
+)
 
 try:
     from .local_settings import *
